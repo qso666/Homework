5b7d8fa4b1334a630558e5563cef9fbc8b66e509
qso666 <example@gmail.com>
2019-05-31 11:21:21 +0300

    4th

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+nano hello_world.cpp#include <iostream>
 #include <string>
 int main(){
     string name;
     std::cin >> name; //ВВОД
     std::cout << "Hello world from " << name; //ВЫВОД
     return 0;
+}#include <iostream>
+#include <string>
+using namespace std;
+cout << "Hello world from " << name;
+return 0;
 }
