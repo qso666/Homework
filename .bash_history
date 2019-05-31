26b071e39a4ab61826759ad59ca48a846f82523d
qso666 <example@gmail.com>
2019-05-31 11:11:51 +0300

    3rd

---
+++ .bash_history
@@ -0,0 +1,7 @@
+su
+apt-get install git
+su
+export GITHUB_USERNAME=qso666
+export GITHUB_EMAIL=vnatus09@gmail.com
+export GIST_TOKEN=3685e87b3f2a096956accedbb4e23133f15ab8a3
+su
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+int main(){
+    string name;
+    std::cin >> name; //ВВОД
+    std::cout << "Hello world from " << name; //ВЫВОД
+    return 0;
+}
